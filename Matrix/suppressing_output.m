adc04c20031ef2941279bd538f3e56bedd60e1d2
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2024-07-28 22:09:48 +0600

    Create suppressing_output.m

---
+++ Matrix/suppressing_output.m
@@ -0,0 +1,10 @@
+A = magic(100);
+A
+
+% if a statement does not fit on one line, use an ellipsis (three periods),
+% ..., followed by Return or Enter to indicate that the statement
+% continues on the next line. For example,
+
+s = 1 - 1/2 + 1/3 - 1/4 + 1/5 - 1/6 + 1/7 ...
+    - 1/8 + 1/9 - 1/10 + 1/11 - 1/12;
+s
